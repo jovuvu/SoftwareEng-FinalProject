c2a3c1f9a1442cb928e6f92e6c2d5a741f85801f
jovuvu <jonathan.vu@colorado.edu>
2014-03-07 14:34:50 -0700

    baseline

--- db/migrate/20140307210422_create_users.rb
+++ db/migrate/20140307210422_create_users.rb
@@ -1,7 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string "name"
+      t.string "email"
+      t.datetime "dob"
+      t.string "gender"
+      t.string "hashed_password"
+      t.text "interests"
+      t.string "quotes"
+      t.string "tvshowsAndmovies"
+      t.string "music"
+      t.text "friends"
       t.timestamps
     end
   end
--- db/migrate/20140307211142_create_posts.rb
+++ db/migrate/20140307211142_create_posts.rb
@@ -1,8 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.integer "postedBy"
+      t.datetime "datePosted"
+      t.text "content"
+      t.integer "parentId"
+      t.text "children"
       t.timestamps
     end
   end
 end
+S
