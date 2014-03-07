f70f5c1492edafc3469c996d0117bfe112f27c87
Dillon Drenzek <dr.enzek@gmail.com>
2014-03-07 16:18:51 -0700

    destroyed existing scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 SoftwareEngFinalProject::Application.routes.draw do
-  resources :posts
-
-  resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
