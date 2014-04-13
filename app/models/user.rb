748d361a7d2e5085cb2386f7b9eee8bf30d2bdc6
Dillon Drenzek <dr.enzek@gmail.com>
2014-04-13 16:35:40 -0600

    email format rspec tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
 	validates :name_first, presence: true, length: {maximum: 50}
 	validates :name_last, presence: true, length: {maximum: 50}
-	validates :email, presence: true
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   	serialize :friends
   	has_many :posts
