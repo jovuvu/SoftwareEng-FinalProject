54cf23663da4c0ddceaedcb9862ab94c638772c6
Dillon Drenzek <dr.enzek@gmail.com>
2014-04-13 16:26:47 -0600

    added email format rspec tests, they fail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	validates :name_first, presence: true, length: {maximum: 50}
-	validates :name_last, presence: true
+	validates :name_last, presence: true, length: {maximum: 50}
 	validates :email, presence: true
 
   	serialize :friends
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
 
   it { should be_valid }
 
+# PRESENCE TESTS
   describe "when name_first is not present" do
   	# test checks to see that a blank first name is caught
   	before { @user.name_first = " "}
@@ -31,9 +32,38 @@ describe User do
   	it { should_not be_valid }
   end
 
+# LENGTH TESTS
   describe "when name_first is too long" do
+  	# test checks length of name_first
   	before {@user.name_first = "a" * 51}
   	it { should_not be_valid }
   end
 
+  describe "when name_last is too long" do
+  	# test checks length of name_last
+  	before {@user.name_last = "a" * 51}
+  	it { should_not be_valid }
+  end
+
+# FORMAT VALIDATION TESTS
+ 	describe "when email format is invalid" do
+ 		it "should be invalid" do
+ 			addresses = %w[user@foo,com user_at_foo.org user@foo.org.
+ 				foo@bar_baz.com foo@bar+baz.com]
+ 			addresses.each do |invalid_address|
+ 				@user.email = invalid_address
+ 				expect(@user).not_to be_valid
+ 			end
+ 		end
+  	end
+
+  	describe "when email format is valid" do
+ 		it "should be valid" do
+ 			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+ 			addresses.each do |valid_address|
+ 				@user.email = valid_address
+ 				expect(@user).to be_valid
+ 			end
+ 		end
+  	end
 end
