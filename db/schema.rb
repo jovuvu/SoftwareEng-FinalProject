70b7da456dfaafb5ca38bb0e1caa5bafb573c59a
Dillon Drenzek <dr.enzek@gmail.com>
2014-04-13 16:45:14 -0600

    added index to user email to further prevent duplicate email addresses, tests still pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411212502) do
+ActiveRecord::Schema.define(version: 20140413224330) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
@@ -33,5 +33,7 @@ ActiveRecord::Schema.define(version: 20140411212502) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
