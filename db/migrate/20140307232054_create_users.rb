2244cb5c8b2d8db82359266bea1636b938d3ea35
Dillon Drenzek <dr.enzek@gmail.com>
2014-04-13 15:50:46 -0600

    rspec for users added, rspec running correctly, no tests written

--- db/migrate/20140307232054_create_users.rb
+++ db/migrate/20140307232054_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string 'name_first'
-      t.string 'name_last'
-      t.string 'email'
-      t.datetime 'dob'
-      t.string 'gender'
+      t.string :name_first
+      t.string :name_last
+      t.string :email
+      t.datetime :dob
+      t.string :gender
       t.string 'password_hash'
       t.text 'interests'
       t.text 'quotes'
