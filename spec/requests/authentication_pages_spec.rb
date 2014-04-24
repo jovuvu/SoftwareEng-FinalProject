3f797e648b0a49f6d23cac4d8e0caa837ff5c415
Dillon Drenzek <dr.enzek@gmail.com>
2014-04-24 12:49:01 -0600

    first two sign-in rspec tests pass

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+    it { should have_content('Sign In') }
+    it { should have_title('Sign In') }
   end
 end
